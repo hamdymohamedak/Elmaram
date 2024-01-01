320e18bbabe77bf2562d03a53a67bb44d038c501
Hamdy Mohamed Askander <121273938+hamdymohamedak@users.noreply.github.com>
2024-01-01 19:24:57 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "ElMaram Pharmacies",
-  description: "El Maram Pharmacies",
+  title: "El-Maram Pharmacies",
+  description: "Locate and find information about El Maram Pharmacies in Victoria, Alexandria, Egypt. Contact information such as telephones and address with a map showing and mobile app",
 };
 
 export default function RootLayout({ children }) {
